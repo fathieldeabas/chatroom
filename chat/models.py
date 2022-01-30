46bcbcd874f5422f2591c27122911440bcb39761
fathi <you@example.com>
2022-01-31 00:51:56 +0200

    add room page

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,21 @@
+from multiprocessing import Value
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+class Room(models.Model):
+    name= models.CharField(max_length=100000)
+
+    def __str__(self):
+        return self.name
+
+class Message(models.Model):
+    value=models.CharField(max_length=100000)
+    date= models.DateTimeField(default= datetime.now, blank=True)
+    user= models.CharField(max_length=60)
+    room= models.CharField(max_length=1000)
+
+    def __str__(self):
+        return 
+
+    def __unicode__(self):
+        return 
